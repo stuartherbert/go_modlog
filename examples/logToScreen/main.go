d3a755377f007bc5c1a8130e0851513c7e26290d
Stuart Herbert <stuart@stuartherbert.com>
2014-11-10 22:41:51 +0000

    New: trivial example code

---
+++ examples/logToScreen/main.go
@@ -0,0 +1,25 @@
+// Copyright (c) 2014-present Stuart Herbert
+// Released under a 3-clause BSD license
+package main
+
+import (
+	log "github.com/stuartherbert/go_modlog"
+)
+
+func main() {
+	log.Traceln("entering")
+	log.Debugln("this is a debug message")
+	log.Infoln("this is an info message")
+	log.Noticeln("this is a notice")
+	log.Warnln("this is a warning")
+	log.Errorln("this is an error")
+	log.Alertln("this is an alert")
+	log.Criticalln("things are going critical now")
+	log.Emergencyln("this is a state of emergency")
+
+	// now we hide some of the messages
+	log.SetOptions(log.SetMinLogLevel(log.InfoLevel))
+	log.Traceln("you shouldn't be able to see this")
+	log.Debugln("you shouldn't be able to see this")
+	log.Infoln("you should be able to see this")
+}
