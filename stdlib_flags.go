fca2df2e0ab571047c3e5dfad7ccbaa70ebba267
Stuart Herbert <stuart@stuartherbert.com>
2014-11-15 23:54:13 +0000

    New: export stdlib log flags for compatibility purposes

---
+++ stdlib_flags.go
@@ -0,0 +1,16 @@
+// Copyright (c) 2014-present Stuart Herbert
+// Released under the 3-clause BSD license
+package modlog
+
+import (
+	"log"
+)
+
+var (
+	Ldate         = log.Ldate
+	Ltime         = log.Ltime
+	Lmicroseconds = log.Lmicroseconds
+	Llongfile     = log.Llongfile
+	Lshortfile    = log.Lshortfile
+	LstdFlags     = log.LstdFlags
+)
